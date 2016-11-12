0dfa890b310fb6d8f8984e4b546672489c1a5f14
David Malary <davidmalary@gmail.com>
2016-11-11 19:27:08 -0500

    scrape() debug

--- server.js
+++ server.js
@@ -14,23 +14,9 @@ var port = process.env.PORT || 8081;
 app.listen(port);
 console.log('Silence please...' + '\n' + 'Curtains up...' + '\n' + 'Server started on: ' + port);
 
-// ================
-// === Cron Job ===
-// ================
-// var job = new CronJob({
-//   cronTime: '',
-//   onTick: function() {
-//     var r = request('http://127.0.0.1:8081/scrape', function(error, response, body){
-//       console.log('==================' + '\n' + 'Scrapper running' + '\n' + '==================');
-//     });
-//   },
-//   start: true
-// });
-
-
-// ===============
-// === Scraper ===
-// ===============
+// =================
+// === Variables ===
+// =================
 var url1 = 'https://en.wikipedia.org/wiki/List_of_towns_and_cities_with_100,000_or_more_inhabitants/cityname:_';
 var url2;
 
@@ -51,16 +37,48 @@ var citiesObj = {
 var json = [];
 var file = '/data/data.json';
 
-var reqCount = 0;
 var itemCount = 0;
 
-for (var n = 0; n < pageLetter.length; n++){
-  url2 = url1 + pageLetter[n];
-  // console.log(url2);
-  if (n === pageLetter.length - 1){
-    console.log("=== URL's generated ===")
-  };
+// =================
+// === Functions ===
+// =================
 
+// === URL Check ===
+var urlCheck = function(){
+  if (n === pageLetter.length - 1){
+    console.log('===============================' + '\n' +
+      "=== Scraper URL's generated ===" +
+      '\n' + '===============================')
+  };
+};
+
+// === Write File ===
+var writeFile = function(){
+  jsonfile.writeFile(file, json, function(err){
+    console.log('====================================' + '\n' +
+    'File created!' + '\n' + 'JSON file located in project Dir' +
+    '\n' + '====================================' );
+  });
+  // ===============
+  // fs.writeFile('cityPopu.json', JSON.stringify(json, null, 4), function(err){
+  //   console.log('====================================' + '\n' + 'File created!' + '\n' + 'JSON file located in project Dir' + '\n' + '====================================' );
+  // });
+  // ===============
+};
+
+// === Cron Job ===
+// var job = new CronJob({
+//   cronTime: '',
+//   onTick: function() {
+//     var r = request('http://127.0.0.1:8081/scrape', function(error, response, body){
+//       console.log('==================' + '\n' + 'Scrapper running' + '\n' + '==================');
+//     });
+//   },
+//   start: true
+// });
+
+// === Scraper ===
+var scrape = function(){
   app.get('/scrape', function(req, res){
     request(url2, function(err_r, res_r, body){
       if (err_r){
@@ -69,38 +87,36 @@ for (var n = 0; n < pageLetter.length; n++){
         var $ = cheerio.load(body);
         console.log('=== On page: ' + $('span:has(small)').text() + ' ===');
 
-        $('tr:has(td)').each(function(index){
+        $('tr:has(td)').each(function(){
           var data = $(this);
           city = data.find('a')[itemCount].children[0].data;
           country = data.find('a')[itemCount + 1].children[0].data;
           console.log(city + ' -- ' + country);
-          // citiesObj.city = city;
-          // citiesObj.country = country;
-          // json.push(citiesObj);
+          console.log(itemCount);
+          citiesObj.city = city;
+          citiesObj.country = country;
+          json.push(citiesObj);
 
           itemCount += 2;
-          // reqCount++;
         });
       };
 
-      // ==================
-      // === Write File ===
-      // ==================
-      // jsonfile.writeFile(file, json, function(err){
-      //   console.log('====================================' + '\n' +
-      //   'File created!' + '\n' + 'JSON file located in project Dir' +
-      //   '\n' + '====================================' );
-      // });
-
-      // ===============
-      // fs.writeFile('cityPopu.json', JSON.stringify(json, null, 4), function(err){
-      //   console.log('====================================' + '\n' + 'File created!' + '\n' + 'JSON file located in project Dir' + '\n' + '====================================' );
-      // });
-      // ===============
+      writeFile();
 
     }); // end of request
-    res.send('=== Check terminal for status ===');
+    res.send('Check terminal for status');
   }); // end of app.get
-}; // end of pageLetter for loop
+}; // end of scrape()
+
+// ==================
+// === Script Run ===
+// ==================
+for (var n = 0; n < pageLetter.length; n++){
+  url2 = url1 + pageLetter[n];
+  // console.log(url2);
+
+  urlCheck();
+  scrape();
+};
 
 exports = exports.module = app;
