e7b1cfc75e6981864c7e212c8d00f9c3e816ac81
madhat5 <davidmalary@gmail.com>
2020-05-13 12:03:58 -0400

    base setup complete, scraping elements

--- server.js
+++ server.js
@@ -13,20 +13,42 @@ const pagesAlpha = [
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ];
 
-var url1 = 'https://en.wikipedia.org/wiki/List_of_towns_and_cities_with_100,000_or_more_inhabitants/cityname:_';
-
 async function listScrape() {
+  const data = []
 
+  for (var i = 0; i < pagesAlpha.length; i++) {
+    await page.goto('https://en.wikipedia.org/wiki/List_of_towns_and_cities_with_100,000_or_more_inhabitants/cityname:_' + pagesAlpha[i]);
+
+    const html = await page.content();
+    const $ = cheerio.load(html);
+
+    $('#mw-content-text > div > table > tbody > tr').each((i, el) => {
+      const cityEl = $(el).find('');
+      const countryEl = $(el).find('');
+      const cityUrlEl = $(el).find('');
+      const countryUrlEl = $(el).find('');
+
+      const city = $(cityEl).;
+      const country = $(countryEl).;
+      const cityUrl = $(cityUrlEl).;
+      const countryUrl = $(countryUrlEl).;
+
+      const dataRow = {
+        city,
+        country,
+        cityUrl,
+        countryUrl
+      };
+    })
+  }
 }
 
 async function main() {
   const browser = await puppeteer.launch({
-    headless.false
+    headless: false
   });
   const page = await browser.newPage();
 
   const cityList = await listScrape(page);
   console.log(cityList);
 }
-
-
